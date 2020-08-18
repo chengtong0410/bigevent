6186593f1ec267f95952f84d83f665d25ef975e3
ChengTong <839738055@qq.com>
2020-08-18 15:35:30 +0800

    login页面完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,68 @@
+// login页面和reg页面切换
 (function () {
     $("#link_reg").on('click', function () {
-        $(this).parent().hide()
+        $(this).parents('.loginBox').hide()
         $('.regexBox').show()
     })
     $('#link_log').on('click', function () {
-        $(this).parent().hide()
+        $(this).parents('.regexBox').hide()
         $('.loginBox').show()
     })
 })()
+// form用layUI表单校验
+layui.form.verify({
+    pass: [
+        /^[\S]{6,12}$/
+        , '密码必须6到12位，且不能出现空格'
+    ],
+    repwd: function (value) {
+        if (value !== $('.regexBox input[name=password]').val()) {
+            return '请两次密码输入不一致'
+        }
+
+    }
+}
+
+)
+// 注册页面
+$('#form_reg').on('submit', function (e) {
+    e.preventDefault()
+    var datas = {
+        username: $('#form_reg [name=userName]').val(),
+        password: $('#form_reg [name=password]').val()
+    }
+    $.ajax({
+        method: 'post',
+        url: 'http://ajax.frontend.itheima.net/api/reguser',
+        data: datas,
+        success: function (res) {
+            if (res.status !== 0) {
+                console.log(res);
+                return layui.layer.msg(res.message)
+            }
+            layui.layer.msg(res.message)
+            $('#link_log').click()
+            $('#form_reg')[0].reset()
+        }
+    })
+})
+//登录页面
+$('#form_login').on('submit', function (e) {
+    e.preventDefault()
+    console.log($(this).serialize());
+    $.ajax({
+        method: 'post',
+        url: 'http://ajax.frontend.itheima.net/api/login',
+        data: $(this).serialize(),
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            layui.layer.msg('登录成功')
+            localStorage.setItem('token', res.token)
+            location.href = '/index.html'
+
+        }
+
+    })
+})
